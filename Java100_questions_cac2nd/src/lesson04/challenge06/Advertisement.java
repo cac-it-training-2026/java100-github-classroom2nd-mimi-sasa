17e673d04af2b5665bd06635571f0ef3d65ba1d0
miyusa <miyusa@C010423276.zipangu.cac.co.jp>
2026-04-15 09:57:57 +0900

    lesson2チャレンジ8まで完了

---
+++ Java100_questions_cac2nd/src/lesson04/challenge06/Advertisement.java
@@ -0,0 +1,24 @@
+package lesson04.challenge06;
+
+public class Advertisement {
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		/*
+		処理の内容：募集要項の表示(以下の項目を表示し、
+		                   詳細及び項目の追加等は自由とする)
+		
+		    ①店員募集のメッセージ
+		    ②勤務時間
+		    ③時給
+		    ④待遇（制服の貸与など）
+		*/
+
+		System.out.println("【ポエール・ネルメ】 従業員募集");
+		System.out.println("\n\t勤務時間 : 9:00 ～ 17:00");
+		System.out.println("\t  時給     : 1500円");
+		System.out.println("\t  待遇     : 制服貸与あり、通勤手当あり");
+
+		System.out.println("\n\n\tアットホームな職場です。");
+
+	}
+}
