3cd3bcd0379939665b7744c60b3c3bd00c6bf3c7
miyusa <miyusa@C010423276.zipangu.cac.co.jp>
2026-04-22 17:15:20 +0900

    レッスン２チャレンジ９からレッスン４チャレンジ７まで完了

--- Java100_questions_cac2nd/src/lesson03/challenge10/Explorer.java
+++ Java100_questions_cac2nd/src/lesson03/challenge10/Explorer.java
@@ -75,14 +75,38 @@ package lesson03.challenge10;
 
 public class Explorer {
 
-        public static void main(String[] args) {
+	public static void main(String[] args) {
 
-                System.out.println("隊長：");
-                System.out.println("滝の前に着いたよ！\n");
+		System.out.println("隊長：");
+		System.out.println("滝の前に着いたよ！\n");
 
+		//ここにfor文のネスト、if文を利用した処理を記述する。
+		for (int i = 13; i < 17; i++) {
 
-                //ここにfor文のネスト、if文を利用した処理を記述する。
+			for (int j = 0; j < 60; j++) {
 
+				if (i == 13 && j == 0) { //最初
+					j = 30;
+				}
 
-        }
+				if (i == 16 && j == 30) { //16時30にになったら
+					System.out.println("隊長：");
+					System.out.println("16:30 時間になったよ\n");
+					System.out.println("滝の水流が弱くなりました。\n");
+					System.out.println("やったー！宝物だー！");
+					break;
+				}
+
+				if (j % 15 == 0) {
+					System.out.println("隊長：");
+					System.out.printf("%d:%02d 待ち遠しいな～%n%n", i, j); //時間表示
+				}
+			}
+
+			if (i == 16) {
+				break;
+			}
+		}
+
+	}
 }
--- Java100_questions_cac2nd/src/lesson04/challenge02/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge02/WarehouseManager.java
@@ -95,23 +95,24 @@ public class WarehouseManager {
 
 		System.out.print("データ型を選んでください（1...文字、2...文字列、3...数値）＞");
 
-
 		//ここに入力処理を記述する。
-
+		String str = br.readLine();
+		int type = Integer.parseInt(str);
 
 		System.out.print("\n要素数を選んでください（1...1個、2...2個、3...3個）＞");
 
-
 		//ここに入力処理を記述する。
-
+		String str2 = br.readLine();
+		int data = Integer.parseInt(str2);
 
 		boolean errFlag = false;
 
+		//ここに入力値の範囲チェック処理を記述する。1より小さくて3より大きかったら×
+		if ((type < 1 || 3 < type) || (data < 1 || 3 < data)) {
+			errFlag = true;
+		}
 
-		//ここに入力値の範囲チェック処理を記述する。
-
-
-		if (!errFlag) {
+		if (!errFlag) { //範囲内の場合
 			System.out.println("\nZ先輩：");
 			System.out.println("中に入れる値はおまかせという事でよろしいですね。");
 			System.out.println("ご注文を承りました。\n");
@@ -126,10 +127,52 @@ public class WarehouseManager {
 			String[] strArray = null;
 			int[] intArray = null;
 
-
 			//ここに入力値による分岐および配列要素数の確定、
 			//値の代入処理を記述する。
 
+			if (type == 1) { //文字
+				if (data == 1) {
+					charArray = new char[1];
+					charArray[0] = 'a';
+				} else if (data == 2) {
+					charArray = new char[2];
+					charArray[0] = 'a';
+					charArray[1] = 'b';
+				} else {
+					charArray = new char[3];
+					charArray[0] = 'a';
+					charArray[1] = 'b';
+					charArray[2] = 'c';
+				}
+			} else if (type == 2) {
+				if (data == 1) {
+					strArray = new String[1];
+					strArray[0] = "abc";
+				} else if (data == 2) {
+					strArray = new String[2];
+					strArray[0] = "abc";
+					strArray[1] = "def";
+				} else {
+					strArray = new String[3];
+					strArray[0] = "abc";
+					strArray[1] = "def";
+					strArray[2] = "ghi";
+				}
+			} else {
+				if (data == 1) {
+					intArray = new int[1];
+					intArray[0] = 1;
+				} else if (data == 2) {
+					intArray = new int[2];
+					intArray[0] = 1;
+					intArray[1] = 2;
+				} else {
+					intArray = new int[3];
+					intArray[0] = 1;
+					intArray[1] = 2;
+					intArray[2] = 3;
+				}
+			}
 
 			System.out.println("Yさん：");
 			System.out.println("...出来ました。\n");
@@ -139,9 +182,15 @@ public class WarehouseManager {
 
 			System.out.println("Yさん：");
 
-
 			//ここに入力値による分岐および配列要素の表示処理を記述する。
 
+			if (type == 1) {
+				System.out.print(charArray[charArray.length - 1]); //０から数えるので
+			} else if (type == 2) {
+				System.out.print(strArray[strArray.length - 1]);
+			} else {
+				System.out.print(intArray[intArray.length - 1]);
+			}
 
 			System.out.println("です。\n");
 
